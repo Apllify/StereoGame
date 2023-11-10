83aa4a76f947d2acd6250dd241c8dabc49eaea06
Apllify <ralilahlou50@gmail.com>
2023-11-10 16:36:01 +0100

    Created Color extension

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using StereoGame.Extras;
+
+namespace StereoGame.Extensions
+{
+
+	/// <summary>
+	/// Provides shorthand for working with colors
+	/// </summary>
+	public static class ColorExtensions
+	{
+		public static void Add(this Color c1, Color c2)
+		{
+			c1.R += c2.R;
+			c1.G += c2.G;
+			c1.B += c2.B;
+		}
+
+		public static Color Added(Color c1, Color c2)
+		{
+			return new Color(Math.Min(255, c1.R + c2.R), Math.Min(255, c1.G + c2.G), Math.Min(255, c1.B + c2.B));
+		}
+
+		public static ColorF ToColorF(this Color c)
+		{
+			return new ColorF(c.R, c.G, c.B);
+		}
+
+	}
+}
---
+++ Extras/ColorF.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Collections;
+
+namespace StereoGame.Extras
+{
+
+	/// <summary>
+	/// An alternative to Monogame.Color which internally represents the channels
+	/// as floats, useful for incrementing colors by small amounts while retaining 
+	/// performance.
+	/// </summary>
+	public struct ColorF
+	{
+		public float Red { get; set; }
+		public float R { get => Red; set => Red = value; }
+
+		public float Green { get; set; }
+		public float G { get => Green; set => Green = value; }
+
+		public float Blue { get; set; }
+		public float B { get => Blue; set => Blue = value; }
+
+
+		public ColorF(float red, float green, float blue)
+		{
+			Red = Math.Clamp(red, 0, 1);
+			Green = Math.Clamp(green, 0, 1);
+			Blue = Math.Clamp(blue, 0, 1);
+		}
+
+		public ColorF(int red, int green, int blue) :
+			this(red / 255f, green / 255f, blue / 255f) { }
+		
+		public static ColorF operator +(ColorF c1, ColorF c2)
+		{
+			return new ColorF(Math.Clamp(c1.Red + c2.Red, 0, 1),
+							  Math.Clamp(c1.Green + c2.Green, 0, 1),
+							  Math.Clamp(c1.Blue + c2.Blue, 0, 1));
+		}
+
+		public static explicit operator Color(ColorF c)
+			=> new Color(c.Red, c.Green, c.Blue);
+
+		public Color ToColor()
+		{
+			return (Color)this;
+		}
+
+	}
+}
